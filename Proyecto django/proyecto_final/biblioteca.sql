4f2bdbf12c49324dad04291091ca6baaf5f7a8f1
Cristina <ana.mendoza@tecsup.edu.pe>
2023-07-06 03:29:01 -0500

    Subiendo Triggers

--- Proyecto django/proyecto_final/biblioteca.sql
+++ Proyecto django/proyecto_final/biblioteca.sql
@@ -1,2 +1,18 @@
 CREATE DATABASE biblioteca
 USE biblioteca
+-----------------------------
+CREATE TRIGGER LIBRO_ACTUALIZAR_AI
+AFTER INSERT ON biblioteca_prestamo
+FOR EACH ROW
+    UPDATE biblioteca_libro
+    SET Cantidad = Cantidad - 1
+    WHERE IdLibro = NEW.IdLibro_id;
+------------------------
+CREATE TRIGGER LIBRO_ACTUALIZAR_AD
+AFTER DELETE ON biblioteca_prestamo
+FOR EACH ROW
+    UPDATE biblioteca_libro
+    SET Cantidad = Cantidad + 1
+    WHERE IdLibro = OLD.IdLibro_id;   
+------------------------------------------------
+
